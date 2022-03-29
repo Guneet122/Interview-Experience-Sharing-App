25dbf4b08cd90c8acf6e484475bdf835bac234ae
Guneet Kaur <guneet@recruitcrm.io>
2022-03-29 20:09:44 +0530

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,195 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1 id="headingTop">Interview Experiences</h1>
+      <ShowExperiences v-for="experience in experiences.slice().reverse()" :experience="experience" :mutableprop="experience.rate" v-bind:key="experience.id"/>    
+
+
+<!-- Form to share experience-->
+
+
+    <section class="enterDiv">
+      <h1 id="heading">Share your Experience</h1>
+
+      <b-field label="Name">
+            <b-input v-model="name"></b-input>
+        </b-field>
+
+        <b-field label="Email">
+            <b-input v-model="email" type="email" maxlength="30">
+            </b-input>
+        </b-field>
+        
+        <b-field label="Gender">
+              <b-radio v-model="gender"
+                name="name"
+                native-value="Male">
+                Male
+              </b-radio>
+              <b-radio v-model="gender"
+                name="name"
+                native-value="Female">
+                Female
+              </b-radio>
+        </b-field>
+
+        <b-field label="Company Name">
+            <b-input v-model="company"></b-input>
+        </b-field>
+        <b-field label="Position Applied">
+            <b-input v-model="position"></b-input>
+        </b-field>
+        <b-field label="Interview Date">
+            <b-datepicker
+                v-model="selected"
+                :show-week-number="showWeekNumber"
+                :locale="locale"
+                placeholder="Click to select..."
+                icon="calendar-today"
+                :icon-right="selected ? 'close-circle' : ''"
+                icon-right-clickable
+                @icon-right-click="clearDate"
+                trap-focus>
+            </b-datepicker>
+        </b-field>
+
+        <b-field label="Interview Questions Level">
+          <b-checkbox v-model="level"
+                native-value="Beginner">
+                Beginner
+            </b-checkbox>
+            <b-checkbox v-model="level"
+                native-value="Intermediate">
+                Intermediate
+            </b-checkbox>
+            <b-checkbox v-model="level"
+                native-value="Hard">
+                Hard
+            </b-checkbox>
+        </b-field>
+
+        <br>
+
+        <b-field label="Offer">
+            <b-select v-model="offer" placeholder="Offer Status">
+                <option>Accepted</option>
+                <option>Rejected</option>
+                <option>Decline</option>
+            </b-select>
+        </b-field>
+        <br>
+        <b-field label="Rate your Interview Experience">
+        <b-rate v-model="rate"
+            icon-pack="fas"  ></b-rate>
+        </b-field>
+        <br>
+
+        <b-field label="Share your Interview Experience">
+            <b-input v-model="userexp" maxlength="200" type="textarea"></b-input>
+        </b-field>
+
+        <b-button type="is-primary" @click="submit">Submit</b-button>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import ShowExperiences from "./components/Show.vue";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{ShowExperiences},
+  data(){
+    return {
+          selected: new Date(),
+          showWeekNumber: false,
+          locale: undefined,
+
+          id:3,
+          name: "",
+          email: "",
+          gender: null,
+          company: "",
+          position: "",
+          level: [],
+          offer: null,
+          rate: 0,
+          userexp:"",
+      experiences: [
+        {
+          id:1,
+          name: "Guneet Kaur",
+          email: "guneet@gmail.com",
+          gender: "Female",
+          company: "Recruit CRM",
+          position: "Associate Software Engineer",
+          date: "2022-02-07",
+          level: ["Intermediate"],
+          offer: "Accepted",
+          rate: 5,
+          userexp:"The interview experience was very good. Candidate must possess basic DSA knowledge."
+        },
+
+        {
+          id:2,
+          name: "Kirat",
+          email: "kirat@gmail.com",
+          gender: "Female",
+          company: "Enest Technologies",
+          position: "Software Engineer",
+          date: "2022-02-09",
+          level: ["Beginner"],
+          offer: "Decline",
+          rate: 3,
+          userexp:"Very easy to crack this interview. Just be familiar with basic python concepts."
+        }
+      ]
+    }
+  },
+
+  methods:{
+    submit(){
+
+      let myDate = new Date(Date.parse(this.selected));
+      let realDate =
+        myDate.getFullYear() +
+        "-" +
+        ("0" + (myDate.getMonth() + 1)).slice(-2) +
+        "-" +
+        ("0" + myDate.getDate()).slice(-2);
+      var newexperience= {
+          id:this.id,
+          name: this.name,
+          email: this.email,
+          gender: this.gender,
+          company: this.company,
+          position: this.position,
+          date: realDate,
+          level: this.level,
+          offer: this.offer,
+          rate: this.rate,
+          userexp:this.userexp
+      }
+      this.experiences.push(newexperience);
+          this.name="",
+          this.email= "",
+          this.gender= null,
+          this.company= "",
+          this.position= "",
+          this.selected= new Date(),
+          this.level= [],
+          this.offer= null,
+          this.rate=0,
+          this.userexp="",
+          this.id++;
+    },
+
+    clearDate () {
+            this.selected = null
+        }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Show.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="showDiv">
+        <ul>
+          <li>Company: <span class="companystyle">{{experience.company}}</span>
+          <span class="positionhead">Position: </span><span class="positionstyle">{{experience.position}}</span>
+          <span class="positionhead">Rating: </span> <b-rate style="display: inline;" v-model="mutableprop"
+            icon-pack="fas" disabled ></b-rate></li>
+            <br>
+          <li>Interview Difficulty: <span  v-for="(level,index) in experience.level" :key="index" style="background-color:#A72693;color: white; border-radius: 5px; padding: 5px;"> {{level}}</span>
+          <span class="positionhead">Offer: </span><span style="background-color:#0A91AB;color: white; border-radius: 5px; padding: 5px;"> {{experience.offer}}</span></li>
+          <br>
+          <li>Interview Experience: <br><br>
+            <span style="background-color:#346751;color: white; font-weight: normal; border-radius: 5px; padding: 5px;">{{experience.userexp}}</span></li>
+          <br>
+          <br>
+          <li>Name: {{experience.name}}</li>
+          <li>Email: {{experience.email}}</li>
+          <li>Gender: {{experience.gender}}</li>
+          <li>Interview Date: {{experience.date}}</li>
+
+          <li>{{experience.upload}}</li>
+        </ul>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowExperiences',
+  props: ["experience"],
+  data(){
+   return{
+     mutableprop: this.experience.rate
+  }
+   } 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
